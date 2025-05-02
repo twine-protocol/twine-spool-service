ea13d127113d840301306a19ccdb01d86bd7e6b1
Jasper Palfree <well.caffeinated@gmail.com>
2025-05-01 20:33:01 -0600

    fix db migrations and add admin panel deployment

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Setup db locally:
 
 ```sh
-wrangler d1 execute spool-dev -e dev --local --file=./schema.sql
+wrangler d1 migrations apply spool-dev -e dev
 ```
 
 Run dev:
@@ -16,29 +16,4 @@ wrangler dev -e dev
 
 ## Production
 
-Setup remotely
-
-```sh
-wrangler d1 execute spool-prod --remote --file=./schema.sql
-```
-
-Put secret key
-
-```sh
-echo some_key_text | wrangler secret put SECRET_KEY_STR
-```
-
-Deploy
-
-```sh
-wrangler deploy
-```
-
-## TODO
-
-- [] Setup github deployment
-- [] Setup deploy to cloudflare button
-- [] Randomness example
-- [] Fix d1 store to use resolver interface
-- [] check put/push api key checking
-
+Deployment is handled by github actions.
---
+++ migrations/0001_create_tables.sql
@@ -0,0 +1,44 @@
+-- Migration number: 0001 	 2025-05-02T02:09:57.373Z
+
+CREATE TABLE IF NOT EXISTS Strands (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  -- Cid bytes (2x varint (9) + 512bit hash (64)) = 18 + 64 = 82
+  cid BINARY(82) UNIQUE NOT NULL,
+  spec TEXT NOT NULL,
+  data BLOB NOT NULL,
+  details JSON DEFAULT '{}',
+  writable BOOLEAN DEFAULT 1 NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_strands_cid ON Strands (cid);
+
+CREATE TABLE IF NOT EXISTS Tixels (
+  cid BINARY(82) UNIQUE NOT NULL,
+  strand INTEGER NOT NULL,
+  idx INTEGER NOT NULL,
+  data BLOB NOT NULL,
+
+  -- Keys
+  PRIMARY KEY (strand, idx),
+  FOREIGN KEY (strand) REFERENCES Strands(id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_tixels_cid ON Tixels (cid);
+
+CREATE TABLE IF NOT EXISTS Registrations (
+  uuid TEXT PRIMARY KEY,
+  email TEXT NOT NULL,
+  status TEXT NOT NULL,
+  strand_cid BINARY(82) UNIQUE NOT NULL,
+  strand BLOB
+);
+
+-- Api Keys
+CREATE TABLE IF NOT EXISTS ApiKeys (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  description TEXT NOT NULL,
+  hashed_key TEXT UNIQUE NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  last_used_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  expires_at TIMESTAMP
+);
